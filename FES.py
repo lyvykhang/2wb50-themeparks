be8cf64ed9b3fad598042589b762736c97436d42
Khang Ly <55149290+lyvykhang@users.noreply.github.com>
2022-03-21 20:18:41 +0100

    Create FES.py

---
+++ FES.py
@@ -0,0 +1,9 @@
+class FES:
+    def __init__(self):
+        self.events = []
+        
+    def add(self, event):
+        heapq.heappush(self.events, event)
+        
+    def next(self):
+        return heapq.heappop(self.events)
