1ec199446d11adb8cf7ee30a06daa2f834b0eaf7
KoenLemmen <koenlemmen@live.nl>
2022-03-22 22:31:44 +0100

    Started with the arrival functions
    Currently not yet implemented in the Sim class. As I wanted to test without having to run everything. I'm not sure what the Max Lambda value should be.

---
+++ ArrivalTests.py
@@ -0,0 +1,62 @@
+from statistics import mean
+from numpy.ma.core import asarray, append, mean, min, max
+from scipy import stats
+from scipy.constants import pi
+import matplotlib.pyplot as plt
+
+from collections import deque
+
+from datetime import datetime
+import time
+
+# TODO: Implement this in Sim.py
+# TODO: Implement other station arrays
+
+# Array for projected customer arrival per hour for stations
+frogPondArr = [450, 300, 275, 285, 310, 320, 280, 260, 290, 315, 385, 415]
+
+def simArrival(stationArr, T):
+    # Function to do Poisson process to get arrival times for simulation
+
+    lam = (mean(stationArr))/60
+
+    arrivalTimes = deque()
+    expDist = stats.expon(scale=1/lam)
+    t = expDist.rvs()
+
+    while t < T:
+        arrivalTimes.append(t)
+        t += expDist.rvs()
+
+    return asarray(arrivalTimes)
+
+def thinning(allArrivals, stationArr):
+    # Function to make the Poisson process non-homogeneous matching the projected customer arrival per hour
+    stationArr = [x / max(stationArr) for x in stationArr]
+    return [(stationArr[int(t/60)]) for t in allArrivals]
+
+def generatePlot(arr):
+    # Generate plot mostly copied from lecture notes
+    ys = range(0, len(arr) + 1)
+    ys = append(ys, [len(arr)])
+    xs = append(append([0], arr), [T])
+    plt.figure()
+    plt.step(xs, ys, 'b', where='post')
+    date = datetime.now().strftime("%Y%m%d_%H%M%S")
+    plt.savefig("results_{0}.png".format(date))
+    plt.clf()
+
+T = 720
+# All Arrivals is homogeneous
+allArrivals = simArrival(frogPondArr, T)
+
+# The next steps are here to make it non-homogeneous
+udist = stats.uniform(0, 1)
+u = udist.rvs(len(allArrivals))
+maxLam = 1 # I am unsure what this value should be currently, it heavily impacts the amount of accepted arrivals
+accept = u * maxLam < thinning(allArrivals, frogPondArr)
+acceptedArrivals = allArrivals[accept]
+
+generatePlot(allArrivals)
+time.sleep(1)
+generatePlot(acceptedArrivals)
