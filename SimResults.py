8db95bd634c3307cd0cf9449606d3fb2566cd520
Khang Ly <55149290+lyvykhang@users.noreply.github.com>
2022-03-28 17:01:11 +0200

    Update SimResults.py

--- SimResults.py
+++ SimResults.py
@@ -2,7 +2,15 @@ class SimResults:
     def __init__(self):
         self.qLengths = [[] for i in range(4)]
         self.eventTimes = [[] for i in range(4)]
+        self.waitingTimes = [[] for i in range(4)]
+        self.unableToBoard = []
     
     def registerQLength(self, value, t, station):
         self.qLengths[station].append(value)
         self.eventTimes[station].append(t)
+        
+    def registerWaitingTime(self, customer, t):
+        self.waitingTimes[customer.arrStation].append(t - customer.arrTime)
+    
+    def registerUnableToBoard(self, value):
+        self.unableToBoard.append(value)
