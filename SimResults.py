be2b53f439ea2d53bbc9b8efa4dc367bf6493a4e
Khang Ly <55149290+lyvykhang@users.noreply.github.com>
2022-03-28 13:54:54 +0200

    Update SimResults.py

--- SimResults.py
+++ SimResults.py
@@ -1,6 +1,8 @@
 class SimResults:
     def __init__(self):
         self.qLengths = [[] for i in range(4)]
+        self.eventTimes = [[] for i in range(4)]
     
-    def registerQLength(self, value, station):
+    def registerQLength(self, value, t, station):
         self.qLengths[station].append(value)
+        self.eventTimes[station].append(t)
